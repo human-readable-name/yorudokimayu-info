720901469b6c751dc94b23e1d6a0032f5396c0e0
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2022-05-21 01:56:36 +0900

    GoogleTagManager組み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
+import GoogleTagManager from '../components/common/GoogleTagManager';
+import { googleTagManagerId } from '../constants/gtm';
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+const App = ({ Component, pageProps }: AppProps) => {
   return <>
+    <GoogleTagManager
+      googleTagManagerId={googleTagManagerId}
+    />
     <Head>
 				<meta	name="viewport"	content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
@@ -29,6 +35,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     />
     <Component {...pageProps} />
   </>
-}
+};
 
-export default MyApp
+export default App;
