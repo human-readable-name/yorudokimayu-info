b2b67e8bcbfaaca05fa27647c400d3c93d3a438d
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2022-05-30 00:40:30 +0900

    運営のTwitterアカウントが不気味すぎるので、修正は公開SpreadSheetでやる

--- src/components/common/PageFooter.tsx
+++ src/components/common/PageFooter.tsx
@@ -4,7 +4,7 @@ const PageFooter: React.FC = () => {
     return <footer className="flex justify-center align-center mt-8">
         <div className="justify-center align-center text-sm">
             アクセス解析にGoogleAnalyticsを使っています。<br/>
-            サイト運営者(Twitter: @ymepwkuBVEvOJKM)<br/>
+            記載情報修正は<span className="underline"><a href="https://docs.google.com/spreadsheets/d/1mpA4WlCHcOBv9KY8MWdiueU4HGsE-7-Nkc8tbtgvZoQ/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Spreadsheet</a></span>へ<br/>
         </div>
     </footer>
 };
