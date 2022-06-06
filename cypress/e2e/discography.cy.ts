d723cabf74871588da002ff24598e51701c8c8c7
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2022-06-07 00:43:42 +0900

    Cypressまわり修正

--- cypress/e2e/discography.cy.ts
+++ cypress/e2e/discography.cy.ts
@@ -14,6 +14,21 @@ describe('Discography(default locale)', () => {
     it('has 3 alternates (default, ja, en)', () => {
         cy.get('link[rel=alternate]').should('have.length', 3);
     });
+    it('has 3 alternates (default, ja, en)', () => {
+        cy.get('link[rel=alternate]').should('have.length', 3);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=x-default]')
+            .filter('link[href=https://kimayu.rocks/discography]')
+            .should('have.length', 1);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=ja]')
+            .filter('link[href=https://kimayu.rocks/discography]')
+            .should('have.length', 1);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=en]')
+            .filter('link[href=https://kimayu.rocks/en/discography]')
+            .should('have.length', 1);
+    });
 });
 
 describe('Discography(en locale)', () => {
@@ -29,5 +44,17 @@ describe('Discography(en locale)', () => {
     });
     it('has 3 alternates (default, ja, en)', () => {
         cy.get('link[rel=alternate]').should('have.length', 3);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=x-default]')
+            .filter('link[href=https://kimayu.rocks/discography]')
+            .should('have.length', 1);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=ja]')
+            .filter('link[href=https://kimayu.rocks/discography]')
+            .should('have.length', 1);
+        cy.get('link[rel=alternate]')
+            .filter('link[hrefLang=en]')
+            .filter('link[href=https://kimayu.rocks/en/discography]')
+            .should('have.length', 1);
     });
 });
