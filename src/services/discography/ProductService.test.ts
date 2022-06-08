806f0b71b1017a4002a29a7085a154423511417d
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2022-06-08 21:31:55 +0900

    TuneCoreのリンクはlangクエリをつける

--- src/services/discography/ProductService.test.ts
+++ src/services/discography/ProductService.test.ts
@@ -4,7 +4,40 @@ test('listProductSummaries', () => {
     expect(
         listProductSummaries("ja")
     ).toEqual(japanese);
+
+    const japaneseCredits = listProductSummaries("ja").flatMap((summary) => summary.credits).join("");
+    expect(
+        japaneseCredits
+    ).toEqual(expect.stringContaining("拠鳥きまゆ"));
+    expect(
+        japaneseCredits
+    ).toEqual(expect.not.stringContaining("KimayuYorudo"));
+
+    const japaneseTuneCoreLinks = listProductSummaries("ja").flatMap((summary) => 
+        summary.storeLinks.filter((linkItem) => linkItem.url.startsWith("https://linkco.re/"))
+    );
+    expect(japaneseTuneCoreLinks.length).toBe(14);
+    japaneseTuneCoreLinks.forEach((link) => {
+        expect(link.url).toContain("lang=ja")
+    });
+    
     expect(
         listProductSummaries("en")
     ).toEqual(english);
+
+    const englishCredits = listProductSummaries("en").flatMap((summary) => summary.credits).join("");
+    expect(
+        englishCredits
+    ).toEqual(expect.stringContaining("KimayuYorudo"));
+    expect(
+        englishCredits
+    ).toEqual(expect.not.stringContaining("拠鳥きまゆ"));
+
+    const englishTuneCoreLinks = listProductSummaries("en").flatMap((summary) => 
+        summary.storeLinks.filter((linkItem) => linkItem.url.startsWith("https://linkco.re/"))
+    );
+    expect(englishTuneCoreLinks.length).toBe(14);
+    englishTuneCoreLinks.forEach((link) => {
+        expect(link.url).toContain("lang=en")
+    });
 });
