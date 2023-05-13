f921276d53ab3a2deb8ec9e4412209179177def0
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2023-05-14 02:55:05 +0900

    2023春M3の情報をNewsから下げる

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -20,59 +20,7 @@ export class NewsMaster {
     }
 }
 
-const newsMasterData: NewsMaster[] = [
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '2023-04-30 M3-2023春 M-11b および Booth にて ファーストアルバム "Say," をリリース'],
-            ['en', '2023-04-30 1st album "Say," will be released at M3-2023spring (M-11b) and Booth']
-        ]),
-        links: [
-            new LinkMaster({
-                name: TranslatableValues.create([
-                    ["ja", "クロスフェードデモ"],
-                    ["en", "Crossfade Demo"],
-                ]),
-                url: TranslatableValues.createUnifiedStatement('https://youtu.be/ZOmXJgo6epE'),
-            }),
-            new LinkMaster({
-                name: TranslatableValues.create([
-                    ["ja", "Say, MV"],
-                    ["en", "Say, music video"],
-                ]),
-                url: TranslatableValues.createUnifiedStatement('https://youtu.be/uP-z-i1J9jE'),
-            }),
-            new LinkMaster({
-                name: TranslatableValues.create([
-                    ["ja", "Savior MV"],
-                    ["en", "Savior music video"],
-                ]),
-                url: TranslatableValues.createUnifiedStatement('https://youtu.be/DJJwNdhzwE4'),
-            }),
-        ],
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '2023-04-30 M3-2023春 パンケーキキャッツ EP サニーソング 1曲担当'],
-            ['en', '2023-04-30 Guest vocalist on pancakecats EP "sunnysong" will be released at M3-2023Spring']
-        ]),
-        links: [
-            new LinkMaster({
-                name: TranslatableValues.create([
-                    ["ja", "特設サイト"],
-                    ["en", "Web site"],
-                ]),
-                url: TranslatableValues.createUnifiedStatement("https://sunnysong.studio.site/"),
-            }),
-            new LinkMaster({
-                name: TranslatableValues.create([
-                    ["ja", "クロスフェードデモ"],
-                    ["en", "Crossfade Demo"],
-                ]),
-                url: TranslatableValues.createUnifiedStatement("https://youtu.be/H5zN6zqcPKE"),
-            }),
-        ],
-    }),
-];
+const newsMasterData: NewsMaster[] = [];
 
 export class InMemoryNewsService implements NewsService {
     listNews(locale: SupportedLocale): NewsItem[] {
