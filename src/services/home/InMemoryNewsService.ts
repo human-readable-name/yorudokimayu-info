7c70675330b93832504b5f7db94b2f5c5d946a93
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2023-05-16 19:17:02 +0900

    TopのNewsにV³出演を記載

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -20,7 +20,23 @@ export class NewsMaster {
     }
 }
 
-const newsMasterData: NewsMaster[] = [];
+const newsMasterData: NewsMaster[] = [
+    new NewsMaster({
+        text: TranslatableValues.create([
+            ["ja", "vortex主催『V³: Virtual Vortex Voyage』(2023-05-28)出演"],
+            ["en", "V³: Virtual Vortex Voyage hosted by vortex (2023-05-28)"],
+        ]),
+        links: [
+            new LinkMaster({
+                url: TranslatableValues.createUnifiedStatement("https://pjblue.zaiko.io/item/356598"),
+                name: TranslatableValues.create([
+                    ["ja", "ZAIKO (アーカイブ視聴 2023年6月27日迄)"],
+                    ["en", "ZAIKO (The archive of the live will be available until June 27)"],
+                ]),
+            })
+        ]
+    })
+];
 
 export class InMemoryNewsService implements NewsService {
     listNews(locale: SupportedLocale): NewsItem[] {
