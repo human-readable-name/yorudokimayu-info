f4146b418c1e55d90d5b165c9f2a4dfb28ed73c0
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2023-06-28 21:27:04 +0900

    ブイサードをNewsから下げ

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -20,23 +20,7 @@ export class NewsMaster {
     }
 }
 
-const newsMasterData: NewsMaster[] = [
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ["ja", "vortex主催『V³: Virtual Vortex Voyage』(2023-05-28)出演"],
-            ["en", "V³: Virtual Vortex Voyage hosted by vortex (2023-05-28)"],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement("https://pjblue.zaiko.io/item/356598"),
-                name: TranslatableValues.create([
-                    ["ja", "ZAIKO (アーカイブ視聴 2023年6月27日迄)"],
-                    ["en", "ZAIKO (The archive of the live will be available until June 27)"],
-                ]),
-            })
-        ]
-    })
-];
+const newsMasterData: NewsMaster[] = [];
 
 export class InMemoryNewsService implements NewsService {
     listNews(locale: SupportedLocale): NewsItem[] {
