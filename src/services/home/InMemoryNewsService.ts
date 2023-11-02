7d597ab9d27ef57a787668c6ae0a71a650577728
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2023-11-03 01:53:48 +0900

    Newsを削除。 Biographyに2023秋M3の情報を追加

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -20,53 +20,7 @@ export class NewsMaster {
     }
 }
 
-const newsMasterData: NewsMaster[] = [
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '2023-10-07 拠鳥きまゆ 1st Oneman Live「PENGUIN A LIVE」'],
-            ['en', '2023-10-07 Kimayu Yorudo 1st solo concert "PENGUIN A LIVE"']
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://www.zan-live.com/live/detail/10311'),
-                name: TranslatableValues.create([
-                    ['ja', '配信 Z-aN (アーカイブは2023-10-31まで視聴可能)'],
-                    ['en', 'Streaming Z-aN (The archive will be available until 2023-10-31)'],
-                ]),
-            }),
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://panora.tokyo/archives/73141'),
-                name: TranslatableValues.create([
-                    ['ja', 'ライブレポート (ネタバレ注意)'],
-                    ['en', 'Report on PANORA (spoiler alert)'],
-                ])
-            })
-        ]
-    }),
-    new NewsMaster({
-        text: TranslatableValues.createUnifiedStatement('2023-10-10 New song "PENGUIN ALIVE"'),
-        links: [
-            LinkMaster.createMusicVideoOnYouTube({id: "jM5AdQGVZ4g"}),
-            LinkMaster.createForTuneCore({id: "vMh6myS4"}),
-            LinkMaster.createForOfficialStore({id: "5153499"}),
-        ]
-    }), 
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '2023-10-21 わくわく！VTuberひろば Vol.35 ミュージックLIVE'],
-            ['en', '2023-10-21 WAKUWAKU VTuber HIROBA Vol.35 music live'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://www.youtube.com/@AkihabaraLab'),
-                name: TranslatableValues.create([
-                    ['ja', '配信 AkihabaraLab YouTubeチャネル'],
-                    ['en', 'Streaming on AkihabaraLab YouTube channel'],
-                ]),
-            })
-        ],
-    })
-];
+const newsMasterData: NewsMaster[] = [];
 
 export class InMemoryNewsService implements NewsService {
     listNews(locale: SupportedLocale): NewsItem[] {
