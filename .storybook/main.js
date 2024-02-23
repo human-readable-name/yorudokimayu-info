70c5c5c192a6c7ebc800b6666fce502514282f56
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2024-02-23 22:12:55 +0900

    トラックリストのコンポーネントの場所間違いを訂正

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,11 @@ module.exports = {
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
     });
+    config.watchOptions = {
+      poll: 1000, // ms
+      aggregateTimeout: 500, // ms
+      ignored: ["node_modules"],
+    };
     return config;
   },
   "framework": {
