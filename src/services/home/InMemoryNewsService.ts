3903aa0e753a03cba178f94da08f84f9992767e6
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2024-04-18 21:51:36 +0900

    Newsにぶいかふぇ♪vol.36を追加

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -20,7 +20,23 @@ export class NewsMaster {
     }
 }
 
-const newsMasterData: NewsMaster[] = [];
+const newsMasterData: NewsMaster[] = [
+    new NewsMaster({
+        text: TranslatableValues.create([
+            ['ja', '2024-05-17 ぶいかふぇ♪ vol.36 出演予定'],
+            ['en', '2024-05-17 Vcafe vol.36'],
+        ]),
+        links: [
+            new LinkMaster({
+                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/6753'),
+                name: TranslatableValues.create([
+                    ['ja', '現地 (秋葉原トークライブBAR from scratch) / 配信 (bitfan) チケット'],
+                    ['en', 'Venue (Akihabara from scracth) / Streaming (bitfan) ticket'],
+                ])
+            })
+        ]
+    })
+];
 
 export class InMemoryNewsService implements NewsService {
     listNews(locale: SupportedLocale): NewsItem[] {
