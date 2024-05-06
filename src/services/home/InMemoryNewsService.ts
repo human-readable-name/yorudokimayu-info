6858904c4cefd84e14f7917e6c1e82d1edc61943
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2024-05-06 21:18:45 +0900

    VRide! Vol.10を追加

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -43,6 +43,32 @@ const newsMasterData: NewsMaster[] = [
                 ])
             })
         ]
+    }),
+    new NewsMaster({
+        text: TranslatableValues.createUnifiedStatement('2024-06-01 Virtual Artist Fes VRide! Vol.10'),
+        links: [
+            new LinkMaster({
+                name: TranslatableValues.create([
+                    ['ja', '公式サイト'],
+                    ['en', 'Official web site'],
+                ]),
+                url: TranslatableValues.createUnifiedStatement('https://www.real-virtual-live.com/vol10'),
+            }),
+            new LinkMaster({
+                name: TranslatableValues.create([
+                    ['ja', '現地 渋谷GRIT'],
+                    ['en', 'Venue Shibuya GRIT'],
+                ]),
+                url: TranslatableValues.createUnifiedStatement('https://l-tike.com/search/?keyword=VRide')
+            }),
+            new LinkMaster({
+                url: TranslatableValues.createUnifiedStatement('https://www.youtube.com/@enogu'),
+                name: TranslatableValues.create([
+                    ['ja', '配信 えのぐYouTubeチャネル'],
+                    ['en', 'Streaming on enogu YouTube channel'],
+                ]),
+            }),
+        ]
     })
 ];
 
