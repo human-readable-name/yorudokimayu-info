b43da0f72a0438fbb126f23e59060d0a22ed58e4
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2024-11-09 14:19:43 +0900

    Newsを更新

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -23,35 +23,6 @@ export class NewsMaster {
 const newsMasterData: NewsMaster[] = [
     new NewsMaster({
         text: TranslatableValues.create([
-            ['ja', '2024-10-10 新曲 人鳥新世界'],
-            ['en', '2024-10-10 New single "JinchoShinsekai"'],
-        ]),
-        links: [
-            LinkMaster.createForTuneCore({id: "mxZ53CBR"}),
-            LinkMaster.createForOfficialStore({id: "6159917"}),
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement("https://youtu.be/Iuliu6nhVdQ"),
-                name: TranslatableValues.createUnifiedStatement("MV"),
-            }),
-        ]
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '2024-10-19 魔ノ姫こあく生誕3Dライブ～Showtime Syndrome～'],
-            ['en', '2024-10-19 Koaku Manohime Birthday 3D Live ~ Showtime Syndrome ~'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://pjblue.zaiko.io/item/366852'),
-                name: TranslatableValues.create([
-                    ["ja", "配信 ZAIKO"],
-                    ["en", "Live straming on ZAIKO"],
-                ])
-            })
-        ]
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
             ['ja', '2024-12-20 ぶいかふぇ♪ vol.74'],
             ['en', '2024-12-20 Vcafe vol.74'],
         ]),
