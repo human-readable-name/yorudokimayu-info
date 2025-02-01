2c7b7153da07a3afb95514f8a517c0476cfd14bd
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2025-02-02 00:10:52 +0900

    ぶいかふぇ♪ vol.85をイベント出演歴へ移動、Newsは開催日の近い順に並び変え

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -23,36 +23,6 @@ export class NewsMaster {
 const newsMasterData: NewsMaster[] = [
     new NewsMaster({
         text: TranslatableValues.create([
-            ['ja', '2025-01-26 ぶいかふぇ♪ vol.85'],
-            ['en', '2025-01-26 Vcafe vol.85'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/9731'),
-                name: TranslatableValues.create([
-                    ['ja', '現地 (秋葉原トークライブBAR from scratch) / 配信 (bitfan) チケット'],
-                    ['en', 'Venue (Akihabara from scracth) / Streaming (bitfan) ticket'],
-                ]),
-            }),
-        ],
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
-            ['ja', '魔ノ姫こあく×拠鳥きまゆ 2man Live「Broaden My World」'],
-            ['en', 'Koaku Manohime and Kimayu Yorudo joint concert "Broaden My World"'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/9958'),
-                name: TranslatableValues.create([
-                    ['ja', '現地 (秋葉原トークライブBAR from scratch) チケット'],
-                    ['en', 'Venue (Akihabara from scracth) ticket'],
-                ]),
-            }),
-        ],
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
             ['ja', '2025-03-08 ぶいかふぇ♪ vol.95'],
             ['en', '2025-03-08 Vcafe vol.95'],
         ]),
@@ -62,6 +32,21 @@ const newsMasterData: NewsMaster[] = [
                 name: TranslatableValues.create([
                     ['ja', '現地 (秋葉原トークライブBAR from scratch) / 配信 (bitfan) チケット'],
                     ['en', 'Venue (Akihabara from scracth) / Streaming (bitfan) ticket'],
+                ]),
+            }),
+        ],
+    }),
+    new NewsMaster({
+        text: TranslatableValues.create([
+            ['ja', '2025-05-17 魔ノ姫こあく×拠鳥きまゆ 2man Live「Broaden My World」'],
+            ['en', '2025-05-17 Koaku Manohime and Kimayu Yorudo joint concert "Broaden My World"'],
+        ]),
+        links: [
+            new LinkMaster({
+                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/9958'),
+                name: TranslatableValues.create([
+                    ['ja', '現地 (秋葉原トークライブBAR from scratch) チケット'],
+                    ['en', 'Venue (Akihabara from scracth) ticket'],
                 ]),
             }),
         ],
