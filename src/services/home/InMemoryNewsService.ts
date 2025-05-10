22c7e624afdf28621278424fd33c3cd7a3333a0c
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2025-05-10 23:07:53 +0900

    ぶいかふぇ♪ vol.112をNewsから下げる

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -23,21 +23,6 @@ export class NewsMaster {
 const newsMasterData: NewsMaster[] = [    
     new NewsMaster({
         text: TranslatableValues.create([
-            ['ja', '2025-05-02 ぶいかふぇ♪ vol.112 '],
-            ['en', '2025-05-02 Vcafe vol.112'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/10721'),
-                name: TranslatableValues.create([
-                    ['ja', '配信 (bitfan) チケット アーカイブは2025-05-08 23:59まで'],
-                    ['en', 'Streaming (bitfan) ticket will be available until 2025-05-08 23:59 (JST)'],
-                ]),
-            }),
-        ],
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
             ['ja', '2025-05-17 魔ノ姫こあく×拠鳥きまゆ 2man Live「Broaden My World」'],
             ['en', '2025-05-17 Koaku Manohime and Kimayu Yorudo joint concert "Broaden My World"'],
         ]),
