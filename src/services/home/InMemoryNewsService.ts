1aa8d6655f923a44491f2bac8d847de358fa421b
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2025-06-01 11:18:53 +0900

    NewsのBroaden My Worldの削除

--- src/services/home/InMemoryNewsService.ts
+++ src/services/home/InMemoryNewsService.ts
@@ -23,21 +23,6 @@ export class NewsMaster {
 const newsMasterData: NewsMaster[] = [    
     new NewsMaster({
         text: TranslatableValues.create([
-            ['ja', '2025-05-17 魔ノ姫こあく×拠鳥きまゆ 2man Live「Broaden My World」'],
-            ['en', '2025-05-17 Koaku Manohime and Kimayu Yorudo joint concert "Broaden My World"'],
-        ]),
-        links: [
-            new LinkMaster({
-                url: TranslatableValues.createUnifiedStatement('https://jcm-event.bitfan.id/events/9958'),
-                name: TranslatableValues.create([
-                    ['ja', '現地 (秋葉原トークライブBAR from scratch) / 配信 (bitfan) チケット'],
-                    ['en', 'Venue (Akihabara from scracth) / Streaming (bitfan) ticket'],
-                ]),
-            }),
-        ],
-    }),
-    new NewsMaster({
-        text: TranslatableValues.create([
             ['ja', '2025-06-14 ぶいかふぇ♪ vol.127'],
             ['en', '2025-06-14 Vcafe vol.127'],
         ]),
