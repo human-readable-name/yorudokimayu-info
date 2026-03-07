26d625e78876cb788b8b43cc5632e30e7a479083
01G271BR9H8WH1VNE8QHKYGDCX <not.getting.a.reply@gmail.com>
2026-03-07 17:40:48 +0900

    chore: next lintコマンドを使わない

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import path from "node:path";
+import globals from 'globals';
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextConfig from "eslint-config-next";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +15,48 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default defineConfig([globalIgnores(["**/*.config.js"]), {
-    extends: compat.extends("next/core-web-vitals", "plugin:storybook/recommended"),
-}]);
+export default defineConfig([
+    globalIgnores(["**/*.config.js"]),
+    js.configs.recommended,
+    ...nextConfig,
+    ...compat.extends("plugin:storybook/recommended"),
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.test.ts", "**/*.test.tsx", "**/*.test.js", "**/*.cy.ts"],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+                ...globals.browser,
+                cy: "readonly",
+            },
+        },
+    },
+    {
+        files: ["**/*.tsx", "**/*.jsx"],
+        languageOptions: {
+            parserOptions: {
+                jsxPragmaFragment: "React",
+            },
+            globals: {
+                React: "readonly",
+            },
+        },
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        ignores: [".storybook/**", "cypress/**"],
+        languageOptions: {
+            parser: tseslint.parser,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "after-used",
+                    argsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
+]);
